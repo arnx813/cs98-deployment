d44ba1c088205888ab628b361d79d35f238656f4
djmatusz15 <dj.m.matusz.25@dartmouth.edu>
2024-11-05 17:35:20 -0500

    Connected to backend. Working on auth now. finished discover + dataset page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import { useState } from "react";
 import Login from "./Login";
 import Discover from "./Discover";
 import { useNavigate } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Profile from "./Profile";
 import { SignupForm } from "./Authentication/signup-auth";
 import { LoginAuth } from "./Authentication/login-auth";
-
-
+import DatasetPage from "./DatasetPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<SignupForm/>} />
+          <Route path="/" element={<SignupForm />} />
           <Route path="/login" element={<LoginAuth />} />
           <Route path="/discover" element={<Discover />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/profile" element={<Profile />} />
+          {/* <Route path="dataset-example" element={<DatasetPage />} /> */}
+          <Route path="/dataset/:id" element={<DatasetPage />} />
         </Routes>
       </Router>
     </div>
