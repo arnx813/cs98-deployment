910fd30978d05c67026d1540732a24c3b8a862f1
Temitayo Ojo <Temitayo.v.ojo.25@dartmouth.edu>
2025-02-24 15:58:43 -0500

    admin form

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { UploadForm } from "./DatasetOperations/Upload-Form";
 import { Toaster } from "./components/ui/toaster";
 import DatasetOperationPage from "./DatasetOperations/page";
 import SellerForm from "./seller-form";
+import { AdminApprovalsTable } from "./admin-form-approval"
 
 // Amplify stuff
 import { Amplify } from "aws-amplify";
@@ -42,6 +43,8 @@ function App() {
           <Route path="seller-form" element={<SellerForm />} />
           <Route path="/checkout/:id" element={<Checkout />} />
           <Route path="/download/:id" element={<Download />} />
+
+          <Route path="/admin" element={<AdminApprovalsTable/>} />
         </Routes>
       </Router>
       <Toaster/>
